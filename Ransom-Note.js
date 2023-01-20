d45c30d314a73412ff9bb88b08b26b2247be6c49
elborracho420 <e.baxter@dark-coin.io>
2023-01-20 10:17:48 -0600

    Fixing ransome note so it passes all checks on submission.

--- Ransom-Note.js
+++ Ransom-Note.js
@@ -26,21 +26,26 @@ Constraints:
 ransomNote and magazine consist of lowercase English letters.
 
 SOLUTION:
-*/
-
 /**
  * @param {string} ransomNote
  * @param {string} magazine
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    let note = ransomNote;
-    let article = magazine;
-
-    if (article.search(note) != -1) {return true}
-    else {return false}
+    //split magazine into an array of substrings composed of individual letters:
+    const myArray = magazine.split(""); 
+    //for loop iterates through ransomNote from index 0 through the end of the string:
+    for (let i = 0; i < ransomNote.length; i++) { 
+        //if loop uses check ransomeNote for the first occurence of the current index 
+        //of myArray, if it returns -1 then it is false 
+        //(if indexOf can't find what it's searching for in the string, it returns a -1 instead of the index where the occurence is located):
+        if (myArray.indexOf(ransomNote[i]) === -1) {  
+            return false;
+        //if indexOf can find what it's searching for, it uses the splice method to remove this from myArray (array of magazine letters),
+        //to avoid using the same letter twice--if the same letter is required multiple times for the ransomNote, and returns true
+        } else {
+            myArray.splice(myArray.indexOf(ransomNote[i]), 1); 
+        }
+    }
+        return true;
 };
-/* use if loop and search() method article for contents of note
-if article does not contain any instances of note, search() will
-return a -1, resulting in false--otherwise the if loop will return
-true. */
