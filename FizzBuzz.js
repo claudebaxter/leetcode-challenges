21c90fc769cbf556b315c49b6195905736e2abe8
elborracho420 <e.baxter@dark-coin.io>
2023-01-20 19:37:57 -0600

    Adding notes to FizzBuzz submission.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -33,15 +33,20 @@ SOLUTION: */
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-
+    //transform n into number range array called numArray starting at index 1:
     let numArr = new Array(n).fill(true).map((e, i) => i+1)
 
+    //transform numArr into array of substrings instead of number values:
     const numToStrArr = numArr.map((number) => {
         return number.toString();
     });
 
+    //assign value of numToStrArr to strArr and initiate with let so value can be changed:
     let strArr = numToStrArr;
 
+    //use a for loop to iteratethrough numArr, and splice to change any index of strArr
+    //if numArr[i] is divisible by 15, 5 or 3, it will change the value at that index to 
+    //"Fizz", "Buzz" or "FizzBuzz" accordingly, and return the modified array.
     for (let i = 0; i <= numArr.length; i++) {
         if (numArr[i] % 15 == 0) {
             strArr.splice(i, 1, "FizzBuzz") 
