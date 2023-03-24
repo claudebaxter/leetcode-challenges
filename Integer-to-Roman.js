5f4a5e9520ee7fda08b3c27859e5a168e28b1c2a
elborracho420 <e.baxter@dark-coin.io>
2023-03-24 17:40:44 -0500

    Had to refactor with a while loop to prevent creating infinite loop with if / else statements.

--- Integer-to-Roman.js
+++ Integer-to-Roman.js
@@ -45,31 +45,30 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.*/
  * @return {string}
  */
 var intToRoman = function(num) {
-    const translator = {
-        1: "I",
-        4: "IV",
-        5: "V",
-        9: "IX",
-        10: "X",
-        40: "XL",
-        50: "L",
-        90: "XC",
-        100: "C",
-        400: "CD",
-        500: "D",
-        900: "CM",
-        1000: "M"
+  const symbols = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" }
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < symbols.length; i++) {
+    while (num >= symbols[i].value) {
+      num -= symbols[i].value;
+      result += symbols[i].symbol;
     }
+  }
 
-    let translation = "";
-    const numString = num.toString().split("").map(digit => parseInt(digit));
-    console.log(numString);
-
-    for (i = 0; i < numString.length; i++) {
-        const digit = numString[i];
-        const place = 10 ** (numString.length - i - 1);
-        const value = digit * place;
-    }
-
-    return translation;
+  return result;
 };
