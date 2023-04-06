b256fb8bc9c648e90855454c48e45f3706357de9
elborracho420 <e.baxter@dark-coin.io>
2023-04-05 23:47:15 -0500

    Adding basic factorialize function.

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,17 @@
+//e.g, n = 5 returns output = 120 (1 * 2 *3 * 4 * 5)
+function factorialize(n) {
+  let result = [];
+  let output = 1;
+
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+  for (let i = 0; i < result.length; i++) {
+    output *= result[i];
+  }
+  return output;
+}
+
 function reverseString(input) {
   return input.split("").reverse().join("");
 }
