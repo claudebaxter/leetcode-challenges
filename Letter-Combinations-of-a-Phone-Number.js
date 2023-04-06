6b079f218721fe657def204328e0bc9bb3405bbb
elborracho420 <e.baxter@dark-coin.io>
2023-04-05 22:11:07 -0500

    Loop through each digit of digits, for each letter loop through each string in output array, concatenating and pushing combinations to output.

--- Letter-Combinations-of-a-Phone-Number.js
+++ Letter-Combinations-of-a-Phone-Number.js
@@ -30,16 +30,29 @@ digits[i] is a digit in the range ['2', '9'].
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    const dic = {
-        "2": ['a', 'b', 'c'],
-        "3": ['d', 'e', 'f'],
-        "4": ['g', 'h', 'i'],
-        "5": ['j', 'k', 'l'],
-        "6": ['m', 'n', 'o'],
-        "7": ['p', 'q', 'r', 's'],
-        "8": ['t', 'u', 'v'],
-        "9": ['w', 'x', 'y', 'z']
+  const dic = {
+    "2": ['a', 'b', 'c'],
+    "3": ['d', 'e', 'f'],
+    "4": ['g', 'h', 'i'],
+    "5": ['j', 'k', 'l'],
+    "6": ['m', 'n', 'o'],
+    "7": ['p', 'q', 'r', 's'],
+    "8": ['t', 'u', 'v'],
+    "9": ['w', 'x', 'y', 'z']
+  };
+  
+  let output = [''];
+  
+  for (let digit of digits) {
+    const letters = dic[digit];
+    const newOutput = [];
+    for (let letter of letters) {
+      for (let str of output) {
+        newOutput.push(str + letter);
+      }
     }
-    let output = [];
-    
+    output = newOutput;
+  }
+  
+  return output;
 };
