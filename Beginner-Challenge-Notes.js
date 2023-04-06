ce28bf1619972310fc09cd3c02de34ddf677c71b
elborracho420 <e.baxter@dark-coin.io>
2023-04-06 00:01:50 -0500

    Find the longest word function.

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,11 @@
+function findLongestWordLength(str) {
+  let inputArr = str.split(" ");
+  let outputArr = inputArr.map(w => w.length);
+  return Math.max(...outputArr);
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog"); //returns 6, jumped is 6 letters long and is the longest word
+
 //e.g, n = 5 returns output = 120 (1 * 2 *3 * 4 * 5)
 function factorialize(n) {
   let result = [];
