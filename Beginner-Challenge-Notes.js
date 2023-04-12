40caac57af7df127a747ac70dbc8cf95c44bdc24
elborracho420 <e.baxter@dark-coin.io>
2023-04-12 14:15:06 -0500

    Truncate a string str (arg 1) based on value of num (arg 2)

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,20 @@
+//Truncate a String
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+//.splice method also works in place of .substring
+
+function truncateString(str, num) {
+  if (num >= str.length) {
+    return str;
+  } else {
+    return str.substring(0, num) + "...";
+  }
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2));
+
+
 //Repeat a String str (1st arg) for num times (2nd arg), return empty string if num is not a positive number
 
 function repeatStringNumTimes(str, num) {
