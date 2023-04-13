9e71931c45a5e056f364633e0a0a0ed9fc57d12b
elborracho420 <e.baxter@dark-coin.io>
2023-04-13 14:32:13 -0500

    Adding one more example of booWho solution from tutorial.

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -12,6 +12,12 @@ function booWho(bool) {
 
 console.log(booWho(false));
 
+//another example of Boo Who:
+
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
+
 
 //Finders Keepers
 //Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
