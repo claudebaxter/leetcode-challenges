6d05d7f407d16a9fd89a0ea727dc62c3d026f99e
elborracho420 <e.baxter@dark-coin.io>
2023-04-28 15:21:00 -0500

    Starting functional programming notes, example of how to utilize a function.

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,31 @@
+/*In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.*/
+
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+console.log(tea4TeamFCC) //returns ['greenTea', 'greenTea', ....] 40 times in array
+// Only change code above this line
+
+//above is beginning of Functional Programming section
+
+
 /*Chunky Monkey
 Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.*/
 
