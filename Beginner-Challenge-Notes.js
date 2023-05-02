69422096a7a77c8f0e17f704d7ebff830cd5b371
elborracho420 <e.baxter@dark-coin.io>
2023-05-02 18:51:57 -0500

    How to implement your own version of map to Array.prototype.myMap().

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,32 @@
+/*Implement map on a Prototype
+As you have seen from applying Array.prototype.map(), or simply map() earlier, the map method returns an array of the same length as the one it was called on. It also doesn't alter the original array, as long as its callback function doesn't.
+In other words, map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.
+You might learn a lot about the map method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.*/
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this))
+    }
+  // Only change code above this line
+  return newArray;
+};
+
+//ALT SOLUTION:
+
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+  this.forEach((element, index, originalArr) =>
+    newArray.push(callback(element, index, originalArr))
+  );
+  // Only change code above this line
+  return newArray;
+};
+
+
 /*Use the map Method to Extract Data from an Array
 So far we have learned to use pure functions to avoid side effects in a program. Also, we have seen the value in having a function only depend on its input arguments.
 This is only the beginning. As its name suggests, functional programming is centered around a theory of functions.
