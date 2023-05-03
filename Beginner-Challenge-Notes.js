b254fa0fc99abee62cc929cc19058cfbe65a05b0
elborracho420 <e.baxter@dark-coin.io>
2023-05-03 18:15:15 -0500

    Create a pure function using the slice method to return an array from the input begin and ending points.

--- Beginner-Challenge-Notes.js
+++ Beginner-Challenge-Notes.js
@@ -1,3 +1,24 @@
+/*Return Part of an Array Using the slice Method
+The slice method returns a copy of certain elements of an array. It can take two arguments, the first gives the index of where to begin the slice, the second is the index for where to end the slice (and it's non-inclusive). If the arguments are not provided, the default is to start at the beginning of the array through the end, which is an easy way to make a copy of the entire array. The slice method does not mutate the original array, but returns a new one.
+Here's an example:
+const arr = ["Cat", "Dog", "Tiger", "Zebra"];
+const newArray = arr.slice(1, 3);
+newArray would have the value ["Dog", "Tiger"].
+Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.*/
+
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+  const newArray = anim.slice(beginSlice, endSlice);
+
+  return newArray;
+
+  // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
+
+
 /*Implement the filter Method on a Prototype
 You might learn a lot about the filter method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
 Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.*/
