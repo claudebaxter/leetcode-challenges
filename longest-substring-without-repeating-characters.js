8067da634143b3033f5c1dde582db29a81f0f949
claudebaxter <e.baxter@dark-coin.io>
2023-06-28 21:38:55 -0500

    Updating solution for longest substring without repeating characters solution.

--- longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let input = s;
     let output = "";
     let subLength = 0;
 
-    for (i = 0; i < input.length; i++) {
-        if (!output.includes(input[i]))
-        {
-            output += input[i]
-        } 
-        else 
-        {
-            let index = output.lastIndexOf(input[i]);
-            output = output.substring(index + 1) + input[i];
+    for (let i = 0; i < s.length; i++) {
+        if (!output.includes(s[i])) {
+            output += s[i];
+        } else {
+            let index = output.lastIndexOf(s[i]);
+            output = output.substring(index + 1) + s[i];
         }
         subLength = Math.max(subLength, output.length);
     }
