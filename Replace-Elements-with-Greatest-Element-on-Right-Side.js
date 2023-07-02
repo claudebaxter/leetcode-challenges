64fe436abb05a862e1d82529c3963a91a4d4e56b
claudebaxter <e.baxter@dark-coin.io>
2023-07-02 10:14:02 -0500

    Replace elements with greatest element on right side solution.

---
+++ Replace-Elements-with-Greatest-Element-on-Right-Side.js
@@ -0,0 +1,63 @@
+/*
+Replace Elements with Greatest Element on Right Side
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+Example 1:
+
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+Explanation: 
+- index 0 --> the greatest element to the right of index 0 is index 1 (18).
+- index 1 --> the greatest element to the right of index 1 is index 4 (6).
+- index 2 --> the greatest element to the right of index 2 is index 4 (6).
+- index 3 --> the greatest element to the right of index 3 is index 4 (6).
+- index 4 --> the greatest element to the right of index 4 is index 5 (1).
+- index 5 --> there are no elements to the right of index 5, so we put -1.
+Example 2:
+
+Input: arr = [400]
+Output: [-1]
+Explanation: There are no elements to the right of index 0.
+ 
+Constraints:
+
+1 <= arr.length <= 104
+1 <= arr[i] <= 105
+*/
+
+*/
+logic:
+- initialize maxValue set to -1
+- traverse arr from end to start
+- - init current = arr[i]
+- - set arr[i] = maxValue;
+- - if current > maxValue, set maxValue to current for next iteration
+- return completed arr
+
+diagram:
+maxValue = 18
+current = 18
+
+        i
+        0   1  2  3  4  5
+arr = [18, 6, 6, 6, 1, -1]
+*/
+
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+    let maxValue = -1;
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let current = arr[i];
+        arr[i] = maxValue;
+        if (current > maxValue) maxValue = current;
+    }
+    
+    return arr;
+};
