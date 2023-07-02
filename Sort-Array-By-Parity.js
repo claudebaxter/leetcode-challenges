11e1166b029a5d12f8386853282282e90056d1ad
claudebaxter <e.baxter@dark-coin.io>
2023-07-02 10:54:33 -0500

    Sort Array by Parity solution (in place).

--- Sort-Array-By-Parity.js
+++ Sort-Array-By-Parity.js
@@ -59,3 +59,46 @@ var sortArrayByParity = function(nums) {
     let output = [...evens, ...odds];
     return output;
 };
+
+
+/*
+(2-pointer in place implementation)
+logic:
+- init left pointer at start & right pointer at end of nums
+- while left is less than right
+- - if nums[left] is even, increment left by 1
+- - if nums[right] is odd, decrement right by 1
+- - if nums[left] is odd and nums[right] is even, swap these elements & increment left++ / decrement right--
+- return nums array after loop ends
+
+diagram:
+left: 2
+right: 1
+
+               r
+                  l
+            0  1  2  3
+let nums = [4, 2, 1, 3]
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        if (nums[left] % 2 === 0) left++;
+        else if (!(nums[right] % 2 === 0)) right--;
+        else {
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            left++;
+            right--;
+        }
+    }
+    
+    return nums;
+};
