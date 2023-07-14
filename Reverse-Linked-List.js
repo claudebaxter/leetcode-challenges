0a700468eb95fd3bb85ffae80fbd2eda7df5d7b9
claudebaxter <e.baxter@dark-coin.io>
2023-07-14 18:08:22 -0500

    Reverse linked list solution.

---
+++ Reverse-Linked-List.js
@@ -0,0 +1,66 @@
+/*
+Reverse Linked List
+
+Solution
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+/*
+logic:
+- initialize prev, current and next pointers
+- while current pointer is not null:
+- - store the next node of current in the next pointer
+- - update next pointer of current to point to previous (reversing direction of pointer)
+- - move the previous and current pointers one step forward in the list (previous = current, current = next)
+- after loop ends, return the previous pointer as the reversed head of the linked list
+
+Approach: 3 pointer approach
+Time complexity: O(n) / linear (n = # nodes in head)
+Space complexity: O(1) / constant
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let previous = null;
+    let current = head;
+    let next = null;
+    
+    while (current) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    
+    return previous;
+};
